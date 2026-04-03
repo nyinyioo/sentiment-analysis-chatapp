51abb970dbafeaa7093e57c8b7a4eb2cc663f200
nyinyioo <nyinyi.ece@gmail.com>
2026-04-03 16:46:09 -0700

    refactor: update sentiment analysis and service

--- backend/ml/sentiment_analysis/sentiment_analysis.py
+++ backend/ml/sentiment_analysis/sentiment_analysis.py
@@ -4,10 +4,16 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import json
 from transformers import pipeline
 
-
-sentiment_analyzer = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment", device=-1)
+sentiment_analyzer = pipeline(
+    "sentiment-analysis",
+    model="cardiffnlp/twitter-roberta-base-sentiment",
+    device=-1
+)
 
 def analyze_sentiment(text):
+    if text is None:
+        return None
+    
     results = sentiment_analyzer(text)
     return results[0]
 
